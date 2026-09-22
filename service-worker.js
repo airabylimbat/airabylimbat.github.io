f9764322dec7057832fdd4bafe7d7bc4a202b4d0
airabylimbat <airabylimbat@gmail.com>
2026-09-22 09:57:29 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,82 @@
+const CACHE_NAME = "aira-cache-v1";
+
+const APP_SHELL = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./qrcode.min.js",
+    "./logo_1.png",
+    "./logo_2.png"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(APP_SHELL))
+        .then(() => self.skipWaiting())
+    );
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys =>
+
+            Promise.all(
+
+                keys
+                .filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+
+            )
+
+        ).then(() => self.clients.claim())
+
+    );
+
+});
+
+self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") return;
+
+    event.respondWith(
+
+        fetch(event.request)
+        .then(response => {
+
+            if (
+                response &&
+                response.status === 200 &&
+                response.type === "basic"
+            ) {
+
+                const copy = response.clone();
+
+                caches.open(CACHE_NAME)
+                .then(cache => {
+                    cache.put(event.request, copy);
+                });
+
+            }
+
+            return response;
+
+        })
+        .catch(() => {
+
+            return caches.match(event.request)
+            .then(cached => {
+
+                return cached || caches.match("./index.html");
+
+            });
+
+        })
+
+    );
+
+});
